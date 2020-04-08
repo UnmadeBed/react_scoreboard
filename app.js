711cad4e6ada2c72daeb394a15dd94612037af4d
unmadebed <kevin.archer@live.com>
2020-04-08 22:44:10 +0100

    add components

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
 
-const desc = 'I just learned how to create a React node and render it into the DOM.'
-const myTitleID = 'main-title';
-const name = 'Kevin';
+const  Header = () => (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className='stats'>Players: 1</span>
+    </header>
+  );
 
-const header = (
-  <header>
-    <h1 id={myTitleID}>{ name }'s First React Element!</h1>
-    <p>{ desc }</p>
-  </header>
-);
+  const Player = () => {
+    return (
+      <div className="player">
+        <span className="player-name">
+          Kevin
+        </span>
+
+        <Counter />        
+      </div>
+    );
+  }
+
+  const Counter = () => {
+    return (
+      <div className="counter">
+        <button className="counter-action-decrement"> - </button>
+        <span className="counter-dash-score">35</span>
+        <button className="counter-action-increment"> + </button>
+      </div>
+    );
+  }
+
+  const App = () => {
+    return (
+      <div className="scoreboard">
+        <Header />
+
+        {/* Players list */}
+        <Player />
+
+      </div>
+    );
+  }
 
 ReactDOM.render(
-  header,
+  <App />,
   document.getElementById('root')
 );
